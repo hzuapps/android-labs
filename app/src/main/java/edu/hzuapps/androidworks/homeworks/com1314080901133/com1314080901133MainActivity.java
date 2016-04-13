1ea6672d1a562f5ae642c18b894fa2faff45a628
Wanghengdan <452950236@qq.com>
2016-04-13 16:41:00 +0800

    #1 #45 lab1

---
+++ app/src/main/java/edu/hzuapps/androidworks/homeworks/com1314080901133/com1314080901133MainActivity.java
@@ -0,0 +1,13 @@
+package com.hzu.viewmicro_blogpictures;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class com1314080901133MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
