1652aa6691c6ada07031c407dbd73c6541d313a7
xinxin06 <952751382@qq.com>
2016-04-13 16:31:15 +0800

    #1 #48 lab1

---
+++ app/src/main/java/edu/hzuapps/androidworks/homeworks/com1314080901106/com1314080901106.java
@@ -0,0 +1,13 @@
+package com.example.chenpeixin.brocaster;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class broadcaster extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_broadcaster);
+    }
+}
