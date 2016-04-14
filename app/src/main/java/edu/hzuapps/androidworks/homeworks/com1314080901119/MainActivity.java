e6708a65c9e8779c0e5512ca835c5197ae96c8f4
lgkk <linguikai@foxmail.com>
2016-04-14 16:39:53 +0800

    实验一

---
+++ app/src/main/java/edu/hzuapps/androidworks/homeworks/com1314080901119/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.linguikai.com1314080901119yansezhaoxiangtong;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
