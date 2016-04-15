de54208b4845fb3e085cbb9f9b2774899d47a601
Calort <2304307672@qq.com>
2016-04-15 22:34:12 +0800

    #1 #173 创建项目

---
+++ app/src/main/java/edu/hzuapps/androidworks/homeworks/com1314080901115/com1314080901115Activity.java
@@ -0,0 +1,14 @@
+package edu.hzuapps.androidworks.homeworks.com1314080901115;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class com1314080901115Activity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_com1314080901115);
+    }
+}
