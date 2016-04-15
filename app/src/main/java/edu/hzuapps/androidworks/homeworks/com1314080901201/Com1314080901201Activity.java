b861166dde531c12d13a8f6991958ccacd09b98c
zengyt <1781262224@qq.com>
2016-04-15 16:06:37 +0800

    #88 #71 实验2

--- app/src/main/java/edu/hzuapps/androidworks/homeworks/com1314080901201/Com1314080901201Activity.java
+++ app/src/main/java/edu/hzuapps/androidworks/homeworks/com1314080901201/Com1314080901201Activity.java
@@ -1,21 +1,21 @@
-package com.example.messagestomailbox;
+package com.example.personalinformation;
 
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
+public class Com1314080901201Activity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_com1314080901201);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+		getMenuInflater().inflate(R.menu.com1314080901201, menu);
 		return true;
 	}
 
