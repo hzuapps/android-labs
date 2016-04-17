43878dd515f57efb1c6144b732b693802ef728ca
zhongdingjun <1178090249@qq.com>
2016-04-17 22:47:03 +0800

    #2 #35  lab-2

---
+++ app/src/main/java/edu/hzuapps/androidworks/homeworks/com1314080901146/Com1314080901146Activity.java
@@ -0,0 +1,15 @@
+package edu.hzuapps.androidworks.homeworks.com1314080901146;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class com1314080901146Activity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_com1314080901146);
+    }
+}
