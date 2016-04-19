416199cf8006408b8b4888d94396be254d942fa5
freedom0 <15767973390@163.com>
2016-04-19 23:17:37 +0800

    Update and rename FullscreenActivity.java to Net1314080903220FullscreenActivity.java

--- app/src/main/java/edu/hzuapps/androidworks/homeworks/net1314080903220/Net1314080903220FullscreenActivity.java
+++ app/src/main/java/edu/hzuapps/androidworks/homeworks/net1314080903220/Net1314080903220FullscreenActivity.java
@@ -14,7 +14,7 @@ import com.example.administrator.address_book.R;
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
-public class FullscreenActivity extends AppCompatActivity {
+public class Net1314080903220FullscreenActivity extends AppCompatActivity {
     /**
      * Whether or not the system UI should be auto-hidden after
      * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
