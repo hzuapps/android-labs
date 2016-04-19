498950ee242e829572032d546864c138d85d47d7
wangrunyu <wry0316@qq.com>
2016-04-19 10:59:39 +0800

    实验二

---
+++ app/src/main/java/edu/hzuapps/androidworks/homeworks/com1314080901229/#55猜拳游戏.java
@@ -0,0 +1,15 @@
+package com.androidworks.hzuapps.finger_guessing;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class Com1314080901229Activity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_com1314080901229);
+
+    }
+}
