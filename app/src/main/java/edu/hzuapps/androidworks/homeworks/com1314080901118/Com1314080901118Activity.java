3c5975193476bc5c6ecf74beba10179458863df3
ks43 <1019389480@qq.com>
2016-04-20 15:09:04 +0800

    #88 #57 Lab2 revise activity

--- app/src/main/java/edu/hzuapps/androidworks/homeworks/com1314080901118/Com1314080901118Activity.java
+++ app/src/main/java/edu/hzuapps/androidworks/homeworks/com1314080901118/Com1314080901118Activity.java
@@ -3,11 +3,11 @@ package com.hzu.lxh.mencard;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MenCard1314080901118 extends AppCompatActivity {
+public class Com1314080901118Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_men_card1314080901118);
+        setContentView(R.layout.activity_com1314080901118);
     }
 }
