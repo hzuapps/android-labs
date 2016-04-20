8cbe5b8088c6bc90f66986157a50cbadbe20581e
freedom0 <15767973390@163.com>
2016-04-20 09:31:03 +0800

    Update Net1314080903220Activity.java

--- app/src/main/java/edu/hzuapps/androidworks/homeworks/net1314080903220/Net1314080903220Activity.java
+++ app/src/main/java/edu/hzuapps/androidworks/homeworks/net1314080903220/Net1314080903220Activity.java
@@ -1,1 +1,13 @@
-hello world !
+package edu.hzuapps.androidworks.homeworks.net1314080903220;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Net131480903220Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_net131480903220);
+    }
+}
