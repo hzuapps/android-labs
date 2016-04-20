e4323f52905d8500e8ffad9a10bf93838a5cf2fe
15767978771hxc <2929494460@QQ.COM>
2016-04-20 10:22:39 +0800

    实验一

--- app/src/main/java/edu/hzuapps/androidworks/homeworks/com1314080901113/Com1314080901113Activity.java
+++ app/src/main/java/edu/hzuapps/androidworks/homeworks/com1314080901113/Com1314080901113Activity.java
@@ -1,4 +1,4 @@
-package edu.hzuapps.androidworks.homeworks.com1314080901103;
+package edu.hzuapps.androidworks.homeworks.com1314080901113;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -9,12 +9,12 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class Com1314080901113Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_com1314080901113);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
