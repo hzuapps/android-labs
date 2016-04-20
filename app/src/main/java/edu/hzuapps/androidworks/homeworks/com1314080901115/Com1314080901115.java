7aac71bdd2e9abae3527d6108593d36c5130ff93
Calort <2304307672@qq.com>
2016-04-20 13:29:27 +0800

    #1 #173 lab1

--- app/src/main/java/edu/hzuapps/androidworks/homeworks/com1314080901115/Com1314080901115.java
+++ app/src/main/java/edu/hzuapps/androidworks/homeworks/com1314080901115/Com1314080901115.java
@@ -1,9 +1,9 @@
-package com.hzu.com1314080901115;
+package com.example.administrator.justtext;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Com1314080901115Activity extends AppCompatActivity {
+public class Com1314080901115 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
