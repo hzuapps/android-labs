132ca4f544ba6bfdf8b66f678338cec46c95e62d
hello <hello@example.com>
2016-04-20 09:47:55 +0800

    #1 #07 lab1b11

---
+++ app/src/main/java/edu/hzuapps/androidworks/homeworks/net1314080903207/Net1314080903207Activity.java
@@ -0,0 +1,15 @@
+package edu.hzuapps.androidworks.homeworks.net13124080903207;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class Net13124080903207Activity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_com1314080901103);
+    }
+}
