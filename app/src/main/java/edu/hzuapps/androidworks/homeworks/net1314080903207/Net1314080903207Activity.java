714a6e472eb79f52a5f61ca7d98db1414fc77caa
woyizhidouz <1319078701@qq.com>
2016-04-20 12:55:35 +0800

    Update Net1314080903207Activity.java

--- app/src/main/java/edu/hzuapps/androidworks/homeworks/net1314080903207/Net1314080903207Activity.java
+++ app/src/main/java/edu/hzuapps/androidworks/homeworks/net1314080903207/Net1314080903207Activity.java
@@ -2,14 +2,14 @@ package edu.hzuapps.androidworks.homeworks.net13124080903207;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Window;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class Net13124080903207Activity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.activity_com1314080901103);
-    }
+   @Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
 }
