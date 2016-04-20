50696066d2f90a0776d7d8fb1c7f473c81100a61
lgkk <linguikai@foxmail.com>
2016-04-20 15:12:51 +0800

    #1 #158 lab1

--- app/src/main/java/edu/hzuapps/androidworks/homeworks/com1314080901119/Com1314080901119Activity.java
+++ app/src/main/java/edu/hzuapps/androidworks/homeworks/com1314080901119/Com1314080901119Activity.java
@@ -1,13 +1,13 @@
-package com.example.linguikai.com1314080901119yansezhaoxiangtong;
+package com.example.linguikai.com1314080901119;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Com1314080901119Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_com1314080901119);
     }
 }
