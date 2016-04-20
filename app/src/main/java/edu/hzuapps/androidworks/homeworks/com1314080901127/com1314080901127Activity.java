5a37857b314ea2d90e0b6a0d7dc1941b62f4007d
phmmm <943330257@qq.com>
2016-04-20 16:18:36 +0800

    #88 #193 lab2--

---
+++ app/src/main/java/edu/hzuapps/androidworks/homeworks/com1314080901127/com1314080901127Activity.java
@@ -0,0 +1,13 @@
+﻿package edu.hzuapps.androidworks.homeworks.com1314080901127;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class com1314080901127Activity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_com1314080901127);
+    }
+}
