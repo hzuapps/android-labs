d425b0b90d797cb03216fb27a19cb1b242b658c4
1314080901137 <417015301@qq.com>
2016-04-20 16:10:41 +0800

    #88 #167 Labs1 and Labs2 1314080901137

---
+++ edu/hzuapps/androidworks/homeworks/com1314080901137/com1314080901137Activity.java
@@ -0,0 +1,14 @@
+package edu.hzuapps.androidworks.homeworks.com1314080901137;  
+import android.app.Activity;  
+import android.os.Bundle;  
+import android.view.Window;  
+ 
+public class com1314080901137Activity extends Activity {  
+ 
+   @Override  
+   protected void onCreate(Bundle savedInstanceState) {  
+      super.onCreate(savedInstanceState);  
+      requestWindowFeature(Window.FEATURE_NO_TITLE);  
+       setContentView(R.layout.activity_com1314080901137);  
+  }  
+}  
