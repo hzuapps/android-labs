89378c246413697419143f59bccfcfdfa28bef7f
WCTNOC <1578979727@qq.com>
2016-04-22 23:47:30 +0800

    #1 #125 实验1

---
+++ app/src/main/java/edu/hzuapps/androidworks/homeworks/net1314080903112/MainActivity_net1314080903112.java
@@ -0,0 +1,16 @@
+package edu.hzuapps.androidworks.homeworks.net1314080903112;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class MainActivity_net1314080903112 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_net1314080903112);
+    }
+}
+
