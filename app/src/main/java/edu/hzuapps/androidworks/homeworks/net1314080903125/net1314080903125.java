6920acf88e7b700a5b28372d25f2c56cdd89d045
ZaakLim <245500710@qq.com>
2016-04-22 12:32:41 +0800

    #1 #82 lab1

---
+++ app/src/main/java/edu/hzuapps/androidworks/homeworks/net1314080903125/net1314080903125.java
@@ -0,0 +1,13 @@
+package edu.hzuapps.androidworks.homeworks.net1314080903125;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class net1314080903125 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload);
+    }
+}
