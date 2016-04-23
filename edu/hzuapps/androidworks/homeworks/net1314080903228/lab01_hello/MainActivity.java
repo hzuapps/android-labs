71ca7088a438ed1bd1170c4112cde77ae698821b
lin <15767973204@163.com>
2016-04-23 13:40:49 +0800

    提交作业，hello

---
+++ edu/hzuapps/androidworks/homeworks/net1314080903228/lab01_hello/MainActivity.java
@@ -0,0 +1,23 @@
+package edu.hzuapps.androidworks.homeworks.net1314080903133.lab01_hello;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+}
