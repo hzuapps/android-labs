973b6b31b49b09e4c0729dba5bad9a4be2e9b864
ZaakLim <245500710@qq.com>
2016-04-26 19:49:03 +0800

    #1 #82 Lab1

--- app/src/main/java/edu/hzuapps/androidworks/homeworks/net1314080903125/net1314080903125.java
+++ app/src/main/java/edu/hzuapps/androidworks/homeworks/net1314080903125/net1314080903125.java
@@ -9,33 +9,9 @@ import android.widget.Button;
 
 public class Net1314080903125 extends AppCompatActivity {
 
-    private static int CAMERA_REQUEST_CODE = 1;
-    private static int GALLERY_REQUEST_CODE = 2;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.net1314080903125);
-        setTitle("net1314080903125");
-
-        Button btn_camera = (Button) findViewById(R.id.btn_camera);
-        btn_camera.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(intent, CAMERA_REQUEST_CODE);
-            }
-        });
-
-        Button btn_gallery = (Button)findViewById(R.id.btn_gallery);
-        btn_gallery.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-                intent.setType("image/*");
-                startActivityForResult(intent, GALLERY_REQUEST_CODE);
-            }
-        });
     }
-
-
-
 }
