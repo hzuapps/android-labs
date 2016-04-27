6822dd1bf40a27adde67810f7d9697775f636eaf
LWJ00 <335757314@qq.com>
2016-04-27 10:57:19 +0800

    #1 #142 实验1

---
+++ app/src/main/java/edu/hzuapps/androidworks/homeworks/net1314080903114/net1314080903114_MainActivity.java
@@ -0,0 +1,16 @@
+package edu.hzuapps.androidworks.homeworks.net1314080903114;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class net1314080903114_MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+       
+    }
+
+}
