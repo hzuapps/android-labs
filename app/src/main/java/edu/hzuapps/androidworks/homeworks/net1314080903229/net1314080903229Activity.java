e4971d89069337ee0950686dc3d50c496e423799
Perkin-liu <1161760497@qq.com>
2016-04-27 09:47:21 +0800

    #1 #135 lab-1

---
+++ app/src/main/java/edu/hzuapps/androidworks/homeworks/net1314080903229/net1314080903229Activity.java
@@ -0,0 +1,13 @@
+package edu.hzuapps.androidworks.homeworks.net1314080903220;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Net131480903229Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_net131480903229);
+    }
+}
