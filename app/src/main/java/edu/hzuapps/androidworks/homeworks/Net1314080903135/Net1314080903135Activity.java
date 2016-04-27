0bb0ee32b960f70269716bfeb24586130d83cb5f
XYyang35 <511401610@qq.com>
2016-04-27 09:18:30 +0800

    实验一
    #1  #141  实验一

--- app/src/main/java/edu/hzuapps/androidworks/homeworks/Net1314080903135/Net1314080903135Activity.java
+++ app/src/main/java/edu/hzuapps/androidworks/homeworks/Net1314080903135/Net1314080903135Activity.java
@@ -16,4 +16,4 @@ public class Net1314080903135Activity extends Activity {
          // Inflate the menu; this adds items to the action bar if it is present.
          getMenuInflater().inflate(R.menu.main, menu);
          return true;
-    }
+     }
