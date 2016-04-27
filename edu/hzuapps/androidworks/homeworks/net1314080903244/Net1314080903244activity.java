094c780b1eb190f56430741910fc6a096b2a3970
CanaceYang <943546561@qq.com>
2016-04-27 16:25:02 +0800

    #1 #39 lab1

--- edu/hzuapps/androidworks/homeworks/net1314080903244/Net1314080903244activity.java
+++ edu/hzuapps/androidworks/homeworks/net1314080903244/Net1314080903244activity.java
@@ -1,52 +1,199 @@
-package com.example.administrator.myapplication;
+package edu.hzuapps.androidworks;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+// java.lang.*
 
-public class Net1314080903244activity extends AppCompatActivity {
+import android.widget.TextView;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+import java.math.BigInteger;
+import java.util.Date;
+import java.io.File;
+import java.util.StringTokenizer;
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+import edu.hzuapps.androidworks.demo.StaticMethods;
+
+import static edu.hzuapps.androidworks.demo.StaticMethods.*;
+
+/**
+ * javadoc
+ * Created by zengsn on 16/3/11.
+ */
+public class Clazz {
+
+    protected int i = 0;
+    private float f = 0.0f;
+    boolean b = false;
+
+
+    String str = "abc def ghi";
+    Integer ii = null;
+
+    StringBuffer strBuff = null;
+    StringBuilder strBui = null;
+
+
+    Clazz1 mClazz1 = new Clazz1();
+
+    int[] arr = new int[10];
+
+    public Clazz() {
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public Clazz(int i) {
+        this.i = i;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    void func() {
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+
+        StringTokenizer strTok = new StringTokenizer(str);
+        while (strTok.hasMoreTokens()) {
+            String token = strTok.nextToken();
         }
 
-        return super.onOptionsItemSelected(item);
+        int bigInt = 1111111111;
+        BigInteger bi = new BigInteger("111111111111111111111111111111");
+
+        Integer iiii = 1;
+        Float fffff = 0.0f;
+        Boolean bbbbb = true;
+
+        System.out.print("");
+        System.currentTimeMillis();
+
+        int iii = MAX;
+
+        Date date = new Date();
+
+        File file = new File("");
+
+        if (b) {
+
+        }
+
+        for (int j = 0; j < 10; j++) {
+
+        }
+
+        while (b) {
+            ;
+        }
+
+        Class oneClass = this.getClass();
+
+    }
+
+    public static void main(String[] args) throws ClassNotFoundException {
+        int i = 1;
+        int j = 2;
+        j = 0;
+        try {
+            int k = i / j;
+            //
+            //
+        } catch (RuntimeException e) {
+            // unchecked exception
+            throw new MyRuntimeException();
+        }
+        Clazz clazz = new Clazz();
+
+        Class oneClass = Class.forName("edu.hzuapps.androidworks.Clazz");
+        oneClass = Clazz.class;
+        // Method, Field, Contructor, Package,
+        try {
+            Object object = oneClass.newInstance();
+            if (object instanceof  Clazz) {
+
+            }
+            clazz = (Clazz) object; // ClassCastException
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
+
+        Object object1 = clazz;
+        clazz.func();
+
+
+        try {
+            Thread.sleep(1000*3);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        Thread current = Thread.currentThread();
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.print(">>>");
+            }
+        });
+        thread.run();
+
     }
 }
+
+class Clazz1 {
+    //private int i;
+    protected int j;
+    int k;
+    public int l;
+
+
+    void func() {
+        Clazz clazz = new Clazz();
+    }
+
+    public int getI() {
+        return 0;
+    }
+
+    public int getJ() {
+        return j;
+    }
+
+    public int getK() {
+        return k;
+    }
+
+    public int getL() {
+        return l;
+    }
+
+    public void setJ(int j) {
+        this.j = j;
+    }
+
+    public void setK(int k) {
+        this.k = k;
+    }
+
+    public void setL(int l) {
+        this.l = l;
+    }
+}
+
+class Clazz2 {
+
+    void func() throws MyException {
+        //this.j = 1;
+        throw new MyException();
+    }
+}
+
+class Clazz3 {
+
+    void func() throws MyRuntimeException {
+        //this.j = 1;
+        throw new MyRuntimeException();
+    }
+}
+
+class MyException extends Exception {
+}
+
+class MyRuntimeException extends RuntimeException {
+}
+
