9f07e5b5828715ef525463a851707ea5c39e35a1
HeyLK <374843192@qq.com>
2016-04-27 15:27:38 +0800

    #88 #59 lab2--

--- app/src/main/java/edu/hzuapps/androidworks/homeworks/com1314080901123/com1314080901123Activity.java
+++ app/src/main/java/edu/hzuapps/androidworks/homeworks/com1314080901123/com1314080901123Activity.java
@@ -3,11 +3,11 @@ package com.example.lk.a123;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class com1314080901123Activity extends AppCompatActivity {
+public class Com1314080901123Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main1);
+        setContentView(R.layout.activity_com1314080901123);
     }
 }
