68705770c5a77445057cca66a40d8447af217db7
unknown <1216219258@qq.com>
2016-04-27 09:13:54 +0800

    #1 #145  实验1

---
+++ edu/hzuapps/androidworks/homeworks/net1314080903127/Net1314080903127.java
@@ -0,0 +1,13 @@
+package edu.hzuapps.androidworks.homeworks.net1314080903127;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Net1314080903127 extends Activity{
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+}
