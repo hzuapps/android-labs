f68c613b7ec2cdb3f0174aa95c53921a1220f109
lalamili <377492817@qq.com>
2016-04-27 15:25:43 +0800

    #1 #175 实验1

--- app/src/main/java/edu/hzuapps/androidworks/homeworks/com1314080901114/com1314080901114Activity.java
+++ app/src/main/java/edu/hzuapps/androidworks/homeworks/com1314080901114/com1314080901114Activity.java
@@ -1,48 +1,52 @@
-package edu.hzuapps.androidworks.homeworks;
+﻿package com.example.administrator.myapplication;
 
 import android.os.Bundle;
-import android.util.Log;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.ViewGroup;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 
-import edu.hzuapps.androidworks.R;
-//import edu.hzuapps.androidworks.homeworks.Net1314080903212;
+public class Com1314080901114Activity extends AppCompatActivity {
 
-/**
- * Created by dell on 2016/3/21.
- */
-public class Net1314080903212 extends BackActivity {
-
-    private LinearLayout mTextContainer;
-
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_Com1314080901114);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-//      TextView textView = new TextView(this);
-//      textView.setText("isshe");
-
-        createViewInJava();
-        setContentView(mTextContainer);
-
-//      默认使用XML创建的View
-//      setContentView(R.layout.net1314080903212);
-
-}
-
-    private void createViewInJava() {
-        mTextContainer = new LinearLayout(this);
-        ViewGroup.LayoutParams layoutParams = new ViewGroup.LayoutParams( //
-                ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        mTextContainer.setLayoutParams(layoutParams);
-        mTextContainer.setOrientation(LinearLayout.HORIZONTAL);
-
-        TextView textView = new TextView(this);
-        textView.setText("isshe: Net13140809032122");
-
-        mTextContainer.addView(textView);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_Com1314080901114, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
