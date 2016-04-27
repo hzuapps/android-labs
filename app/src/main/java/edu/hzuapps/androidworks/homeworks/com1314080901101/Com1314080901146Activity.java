7fa6aff010f5bb8278f9ad70af398a0829e5c3ee
zhongdingjun <1178090249@qq.com>
2016-04-27 16:32:33 +0800

    #89 #363 lab-3

--- app/src/main/java/edu/hzuapps/androidworks/homeworks/com1314080901101/Com1314080901146Activity.java
+++ app/src/main/java/edu/hzuapps/androidworks/homeworks/com1314080901101/Com1314080901146Activity.java
@@ -1,13 +1,13 @@
-package edu.hzuapps.androidworks.homeworks.com1314080901101;
+package edu.hzuapps.androidworks.homeworks.Com1314080901146;
 
 import android.app.Activity;
 import android.os.Bundle;
 
-public class com1314080901101Activity extends Activity {
+public class Com1314080901146Activity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_com1314080901101);
+        setContentView(R.layout.activity_Com1314080901146);
     }
 }
