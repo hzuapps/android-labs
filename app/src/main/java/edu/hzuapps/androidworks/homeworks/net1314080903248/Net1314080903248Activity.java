8eaa452d2b12c2bf399794c2fd8615de5f02929a
xunzai <1475002112@qq.com>
2016-04-27 20:03:43 +0800

    #89 完成实验三

--- app/src/main/java/edu/hzuapps/androidworks/homeworks/net1314080903248/Net1314080903248Activity.java
+++ app/src/main/java/edu/hzuapps/androidworks/homeworks/net1314080903248/Net1314080903248Activity.java
@@ -1,15 +1,30 @@
-package edu.hzuapps.androidworks.homeworks.net1314080903247;
+package edu.hzuapps.androidworks.homeworks.net1314080903248;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class Net1314080903247Activity extends Activity {
+/**
+ * Created by xunzai on 2016/4/26.
+ */
+public class FirstActivity extends Activity{
+
+    private Button button1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.activity_net1314080903248);
+        setContentView(R.layout.first_layout);
+        button1 = (Button)findViewById(R.id.button_1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(FirstActivity.this,"You clicked 按钮",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
