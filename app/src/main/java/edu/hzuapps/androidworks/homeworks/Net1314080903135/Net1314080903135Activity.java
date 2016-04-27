06b2575bc0bafc2eef7f99517034bec3679b1aaa
XYyang35 <511401610@qq.com>
2016-04-27 09:14:36 +0800

    Update Net1314080903135Activity.java
    #1  #141  实验1

--- app/src/main/java/edu/hzuapps/androidworks/homeworks/Net1314080903135/Net1314080903135Activity.java
+++ app/src/main/java/edu/hzuapps/androidworks/homeworks/Net1314080903135/Net1314080903135Activity.java
@@ -1,1 +1,19 @@
+package edu.hzuapps.androidworks.homeworks.net1314080903135.;
 
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class Net1314080903135Activity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+         super.onCreate(savedInstanceState);
+         setContentView(R.layout.activity_main);
+     }
+ 
+     @Override
+     public boolean onCreateOptionsMenu(Menu menu) {
+         // Inflate the menu; this adds items to the action bar if it is present.
+         getMenuInflater().inflate(R.menu.main, menu);
+         return true;
+    }
