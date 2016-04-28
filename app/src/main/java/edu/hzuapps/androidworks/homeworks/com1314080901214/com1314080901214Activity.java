14c21625388a20f42c7e92c5144f9a518ad3c6b6
11hks11 <912702351@qq.com>
2016-04-28 15:34:31 +0800

    sy12

---
+++ app/src/main/java/edu/hzuapps/androidworks/homeworks/com1314080901214/com1314080901214Activity.java
@@ -0,0 +1,13 @@
+package com.homeworks.androidworks.hzuapps.edu.desktopmemo;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+public class com1314080901214Activity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_com1314080901214);
+    }
+}
