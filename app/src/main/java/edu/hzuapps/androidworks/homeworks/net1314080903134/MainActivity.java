be5ae5a8a442c105505f12f7e2ea180759fd53f3
贝壳 <448252312@qq.com>
2016-04-28 09:30:30 +0800

    #89 #144 实验23

---
+++ app/src/main/java/edu/hzuapps/androidworks/homeworks/net1314080903134/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.hzuapps.androidworks.homeworks.net1314080903134;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.net1314080903134);
+    }
+}
