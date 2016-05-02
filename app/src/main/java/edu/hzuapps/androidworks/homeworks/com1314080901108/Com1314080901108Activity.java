22911c3798bd3fb41c4177b5fd02af103b56c3b3
czgshy <532247474@qq.com>
2016-05-02 15:52:01 +0800

    #90 #163 界面设置和添加控件

--- app/src/main/java/edu/hzuapps/androidworks/homeworks/com1314080901108/Com1314080901108Activity.java
+++ app/src/main/java/edu/hzuapps/androidworks/homeworks/com1314080901108/Com1314080901108Activity.java
@@ -1,16 +1,14 @@
-package edu.hzuapps.androidworks.homeworks.com1314080901108;
+package com.example.czg.com1314080901108;
 
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Window;
 
-public class Com1314080901108Activity extends BackActivity {
+public class Com1314080901108Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-	requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_com1314080901108);
+        
     }
 }
-
