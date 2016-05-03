0c711d6f1b2a6e9f6617ebb75cdc7905b472b98d
besttip <644546115@qq.com>
2016-05-04 00:07:15 +0800

    #1 #? 实验一

---
+++ app/src/main/java/edu/hzuapps/androidworks/homeworks/net1314080903107/Net1314080903107Activity.java
@@ -0,0 +1,16 @@
+package edu.hzuapps.androidworks.homeworks.net1314080903107;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+import com.example.xuanhang.myapplication.R;
+
+public class Net1314080903107Activity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.net1314080903107);
+    }
+
+}
