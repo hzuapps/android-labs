162f19cafdc9df06f7b9be7b3bd1b8207625c357
WL101ZYF <2687292764@qq.com>
2016-05-04 09:15:08 +0800

    #90 #38 lab4

--- app/src/main/java/edu/hzuapps/androidworks/homeworks/net1314080903101/Net1314080903101activity.java
+++ app/src/main/java/edu/hzuapps/androidworks/homeworks/net1314080903101/Net1314080903101activity.java
@@ -1,52 +1,63 @@
 package edu.hzuapps.androidworks.homeworks.net1314080903101;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
-import android.view.MenuItem;
 
-public class Net1314080903101activity extends AppCompatActivity {
+import java.io.FileNotFoundException;
+
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+public class Net1314080903101Activity extends AppCompatActivity {
+
+    private Uri imageUri;
+    private ImageButton imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_net1314080903101);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        imageView = (ImageButton) findViewById(R.id.imageView);
+        imageUri = Uri.parse("file:///sdcard/temp.jpg");
+
+        Intent intent;
+        intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);      //调用照相机
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);        //指定照相机拍到的照片原图存放位置
+        startActivityForResult(intent, 1);
+
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_net1314080903101, menu);
+        getMenuInflater().inflate(R.menu.menu_main_net1314080903101, menu);
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+// onActivityResult需要按提示修改
+        if(resultCode == Activity.RESULT_OK)
+        {Bitmap bitmap;
+            try {
+                bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(imageUri));
+                imageView.setImageBitmap(bitmap);
+            } catch (FileNotFoundException e) {
+                // TODO 自动生成的 catch 块
+                e.printStackTrace();
+            }
+
+
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
