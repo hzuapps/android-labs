4f23168337eb33fb5a53fb5b8b8eee92cda04949
HeyLK <374843192@qq.com>
2016-05-04 17:24:35 +0800

    #89 #59 lab3--

--- app/src/main/java/edu/hzuapps/androidworks/homeworks/com1314080901123/Com1314080901123Activity.java
+++ app/src/main/java/edu/hzuapps/androidworks/homeworks/com1314080901123/Com1314080901123Activity.java
@@ -1,13 +1,12 @@
-package edu.hzuapps.androidworks.homeworks.com1314080901123;
+package edu.hzuapps.androidworks.homework.text002;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Com1314080901123Activity extends AppCompatActivity {
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_com1314080901123);
+        setContentView(R.layout.activity_picture);
     }
 }
