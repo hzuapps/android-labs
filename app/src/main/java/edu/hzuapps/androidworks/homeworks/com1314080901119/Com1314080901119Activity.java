fa7538546b313917ba7c74d6121d18b2a8b65a63
lgkk <linguikai@foxmail.com>
2016-05-04 15:48:55 +0800

    #89 #90 #92 #158 lab3&4&6

--- app/src/main/java/edu/hzuapps/androidworks/homeworks/com1314080901119/Com1314080901119Activity.java
+++ app/src/main/java/edu/hzuapps/androidworks/homeworks/com1314080901119/Com1314080901119Activity.java
@@ -1,13 +1,76 @@
-package com.example.linguikai.com1314080901119;
+/*
+ * Copyright (C) 2008 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-import android.support.v7.app.AppCompatActivity;
+package edu.hzuapps.androidworks.homeworks.com1314080901119;
+
+import android.app.ListActivity;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.SimpleCursorAdapter;
 
-public class Com1314080901119Activity extends AppCompatActivity {
+public class Com1314080901119Activity extends ListActivity {
+	public static final int INSERT_ID = Menu.FIRST;
+	
+	private int mNoteNumber = 1;
+	private NotesDbAdapter mDbHelper;
+    
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.notepad_list);
+        mDbHelper = new NotesDbAdapter(this);
+        mDbHelper.open();
+        fillData();
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_com1314080901119);
+    public boolean onCreateOptionsMenu(Menu menu) {
+    	boolean result = super.onCreateOptionsMenu(menu);
+        menu.add(0, INSERT_ID, 0, R.string.menu_insert);
+        return result;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+    	switch (item.getItemId()) {
+        case INSERT_ID:
+            createNote();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    
+    private void createNote() {
+
+    }
+    
+    private void fillData() {
+        // Get all of the notes from the database and create the item list
+        Cursor c = mDbHelper.fetchAllNotes();
+        startManagingCursor(c);
+
+        String[] from = new String[] { NotesDbAdapter.KEY_TITLE };
+        int[] to = new int[] { R.id.text1 };
+        
+        // Now create an array adapter and set it to display using our row
+        SimpleCursorAdapter notes =
+            new SimpleCursorAdapter(this, R.layout.notes_row, c, from, to);
+        setListAdapter(notes);
     }
 }
