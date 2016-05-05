3ed9a0372b35bcdcfec44b69cb7699cf6a4f0d9a
DaveXander <1348061165@qq.com>
2016-05-05 10:41:15 +0800

    #88 #89

---
+++ Net1314080903243MainActivity.java
@@ -0,0 +1,25 @@
+package edu.hzuapps.androidworks.homeworks.net1314080903243;
+
+import edu.hzuapps.androidworks.homeworks.net1314080903243gen.R;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class Net1314080903243MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_net1314080903243_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.net1314080903243_main, menu);
+        return true;
+    }
+    
+}
