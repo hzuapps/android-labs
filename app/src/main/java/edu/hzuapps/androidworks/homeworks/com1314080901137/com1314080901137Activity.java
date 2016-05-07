48465d320156d75f74bdf787d46c8b2f54def8c1
1314080901137 <417015301@qq.com>
2016-05-07 16:21:54 +0800

    #89 #167 实验一和实验二的修改 实验三 1314080901137

--- app/src/main/java/edu/hzuapps/androidworks/homeworks/com1314080901137/com1314080901137Activity.java
+++ app/src/main/java/edu/hzuapps/androidworks/homeworks/com1314080901137/com1314080901137Activity.java
@@ -1,14 +1,13 @@
-package edu.hzuapps.androidworks.homeworks.com1314080901137;  
-import android.app.Activity;  
-import android.os.Bundle;  
-import android.view.Window;  
- 
-public class com1314080901137Activity extends Activity {  
- 
-   @Override  
-   protected void onCreate(Bundle savedInstanceState) {  
-      super.onCreate(savedInstanceState);  
-      requestWindowFeature(Window.FEATURE_NO_TITLE);  
-       setContentView(R.layout.activity_com1314080901137);  
-  }  
-}  
+package edu.hzuapps.androidworks.homeworks.com1314080901137;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Com1314080901137Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_com1314080901137);
+    }
+}
