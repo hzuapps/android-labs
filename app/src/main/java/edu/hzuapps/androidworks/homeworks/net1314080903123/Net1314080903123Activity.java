73e8a2bc056bf14882d43a7e334a6fe9b10d732c
linjiejie <799784042@qq.com>
2016-05-10 21:49:43 +0800

    #2 #3 #4 #680 实验2 实验3 实验4

--- app/src/main/java/edu/hzuapps/androidworks/homeworks/net1314080903123/Net1314080903123Activity.java
+++ app/src/main/java/edu/hzuapps/androidworks/homeworks/net1314080903123/Net1314080903123Activity.java
@@ -1,15 +1,65 @@
-package edu.hzuapps.androidworks.homeworks.net1314080903123;
+package net1314080903102.homeworks.androidworks.hzuapps.edu.net1314080903123;
 
-import org.junit.Test;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
-import static org.junit.Assert.*;
+public class Net1314080903123Activity extends Activity implements OnClickListener {
+    TextView dollars;
+    TextView eros;
+    RadioButton dtoe;
+    RadioButton etod;
+    Button convert;
+    @Override
+    protected void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        setContentView(R.layout.activity_net1314080903123);
+        dollars = (TextView)this.findViewById(R.id.dollars);
+        eros = (TextView)this.findViewById(R.id.Eros);
 
-/**
- * To work on unit tests, switch the Test Artifact in the Build Variants view.
- */
-public class Net1314080903123Activity {
-    @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+        dtoe = (RadioButton)this.findViewById(R.id.dtoe);
+        dtoe.setChecked(true);
+        etod = (RadioButton)this.findViewById(R.id.etod);
+
+        convert = (Button)this.findViewById(R.id.convert);
+        convert.setOnClickListener(this);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_net1314080903123, menu);
+        return true;
+    }
+
+    @Override
+    public void onClick(View v) {
+        // TODO Auto-generated method stub
+        if (dtoe.isChecked()) {
+            convertDollarsToEuros();
+        }
+        if (etod.isChecked()) {
+            convertEurosToDollars();
+        }
+    }
+
+    private void convertEurosToDollars() {
+        // TODO Auto-generated method stub
+        double val = Double.parseDouble(eros.getText().toString());
+        // in a real app, we'd get this off the 'net
+        dollars.setText(Double.toString(val / 0.67));
+    }
+
+    private void convertDollarsToEuros() {
+        // TODO Auto-generated method stub
+        double val = Double.parseDouble(dollars.getText().toString());
+        // in a real app, we'd get this off the 'net
+        eros.setText(Double.toString(val*0.67));
+    }
+
 }
