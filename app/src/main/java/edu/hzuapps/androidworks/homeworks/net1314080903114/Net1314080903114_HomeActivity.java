c6e6b07248bbf6be61f4cf34ed87d10c7677dd06
LWJ00 <335757314@qq.com>
2016-05-10 19:38:35 +0800

    #142 #4

---
+++ app/src/main/java/edu/hzuapps/androidworks/homeworks/net1314080903114/Net1314080903114_HomeActivity.java
@@ -0,0 +1,16 @@
+package edu.hzuapps.androidworks.homeworks.net1314080903114;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class Net1314080903114_HomeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_net1314080903114__home);
+    }
+
+
+}
