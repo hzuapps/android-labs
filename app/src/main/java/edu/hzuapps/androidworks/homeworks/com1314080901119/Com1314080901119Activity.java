e47630a9c823daddba3ae64d8a7ee75ec88be27a
lgkk <linguikai@foxmail.com>
2016-05-11 15:31:23 +0800

    #95 #158 lab9

--- app/src/main/java/edu/hzuapps/androidworks/homeworks/com1314080901119/Com1314080901119Activity.java
+++ app/src/main/java/edu/hzuapps/androidworks/homeworks/com1314080901119/Com1314080901119Activity.java
@@ -17,60 +17,131 @@
 package edu.hzuapps.androidworks.homeworks.com1314080901119;
 
 import android.app.ListActivity;
+import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ContextMenu.ContextMenuInfo;
+import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
+import android.widget.AdapterView.AdapterContextMenuInfo;
 
 public class Com1314080901119Activity extends ListActivity {
-	public static final int INSERT_ID = Menu.FIRST;
-	
-	private int mNoteNumber = 1;
-	private NotesDbAdapter mDbHelper;
+    private static final int ACTIVITY_CREATE=0;
+    private static final int ACTIVITY_EDIT=1;
+    
+    private static final int INSERT_ID = Menu.FIRST;
+    private static final int DELETE_ID = Menu.FIRST + 1;
+
+    private NotesDbAdapter mDbHelper;
+    private Cursor mNotesCursor;
     
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.notepad_list);
+        setContentView(R.layout.notes_list);
         mDbHelper = new NotesDbAdapter(this);
         mDbHelper.open();
         fillData();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-    	boolean result = super.onCreateOptionsMenu(menu);
-        menu.add(0, INSERT_ID, 0, R.string.menu_insert);
-        return result;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-    	switch (item.getItemId()) {
-        case INSERT_ID:
-            createNote();
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-    
-    private void createNote() {
-
+        registerForContextMenu(getListView());
     }
     
     private void fillData() {
 
-        Cursor c = mDbHelper.fetchAllNotes();
-        startManagingCursor(c);
-
-        String[] from = new String[] { NotesDbAdapter.KEY_TITLE };
-        int[] to = new int[] { R.id.text1 };
+        mNotesCursor = mDbHelper.fetchAllNotes();
+        startManagingCursor(mNotesCursor);
         
 
-        SimpleCursorAdapter notes =
-            new SimpleCursorAdapter(this, R.layout.notes_row, c, from, to);
+        String[] from = new String[]{NotesDbAdapter.KEY_TITLE};
+        
+
+        int[] to = new int[]{R.id.text1};
+        
+
+        SimpleCursorAdapter notes = 
+        	    new SimpleCursorAdapter(this, R.layout.notes_row, mNotesCursor, from, to);
         setListAdapter(notes);
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        menu.add(0, INSERT_ID,0, R.string.menu_insert);
+        return true;
+    }
+    
+    @Override
+    public boolean onMenuItemSelected(int featureId, MenuItem item) {
+        switch(item.getItemId()) {
+        case INSERT_ID:
+            createNote();
+            return true;
+	    }        
+        return super.onMenuItemSelected(featureId, item);
+    }
+
+    @Override
+	public void onCreateContextMenu(ContextMenu menu, View v,
+			ContextMenuInfo menuInfo) {
+		super.onCreateContextMenu(menu, v, menuInfo);
+        menu.add(0, DELETE_ID, 0, R.string.menu_delete);
+	}
+
+    @Override
+	public boolean onContextItemSelected(MenuItem item) {
+		switch(item.getItemId()) {
+    	case DELETE_ID:
+    		AdapterContextMenuInfo info = (AdapterContextMenuInfo) item.getMenuInfo();
+	        mDbHelper.deleteNote(info.id);
+	        fillData();
+	        return true;
+		}
+		return super.onContextItemSelected(item);
+	}
+
+	private void createNote() {
+    	Intent i = new Intent(this, NoteEdit.class);
+    	 startActivityForResult(i, ACTIVITY_CREATE);
+    }
+    
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+    	super.onListItemClick(l, v, position, id);
+        Cursor c = mNotesCursor;
+        c.moveToPosition(position);
+        Intent i = new Intent(this, NoteEdit.class);
+        i.putExtra(NotesDbAdapter.KEY_ROWID, id);
+        i.putExtra(NotesDbAdapter.KEY_TITLE, c.getString(
+                c.getColumnIndexOrThrow(NotesDbAdapter.KEY_TITLE)));
+        i.putExtra(NotesDbAdapter.KEY_BODY, c.getString(
+                c.getColumnIndexOrThrow(NotesDbAdapter.KEY_BODY)));
+        startActivityForResult(i, ACTIVITY_EDIT);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+        Bundle extras = intent.getExtras();
+        switch(requestCode) {
+        case ACTIVITY_CREATE:
+            String title = extras.getString(NotesDbAdapter.KEY_TITLE);
+            String body = extras.getString(NotesDbAdapter.KEY_BODY);
+            mDbHelper.createNote(title, body);
+            fillData();
+            break;
+        case ACTIVITY_EDIT:
+            Long rowId = extras.getLong(NotesDbAdapter.KEY_ROWID);
+            if (rowId != null) {
+                String editTitle = extras.getString(NotesDbAdapter.KEY_TITLE);
+                String editBody = extras.getString(NotesDbAdapter.KEY_BODY);
+                mDbHelper.updateNote(rowId, editTitle, editBody);
+            }
+            fillData();
+            break;
+        }
     }
 }
