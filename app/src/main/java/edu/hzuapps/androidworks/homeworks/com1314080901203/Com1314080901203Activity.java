b0970439d5b277321ea2d7975dcc5c40b580861b
baoleng5980 <15627388597@163.com>
2016-05-12 16:05:42 +0800

    second_commit

--- app/src/main/java/edu/hzuapps/androidworks/homeworks/com1314080901203/Com1314080901203Activity.java
+++ app/src/main/java/edu/hzuapps/androidworks/homeworks/com1314080901203/Com1314080901203Activity.java
@@ -2,33 +2,73 @@ package com.example.com1314080901203;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class Com1314080901203Activity extends Activity {
-
+	
+	private ViewPager mViewPager;
+	private PagerAdapter mPagerAdapter;
+	int[] imgRes = {R.id.id_tab_chatroom_img,R.id.id_tab_contact_img,R.id.id_Messagesender_img};
+	Button send;
+	ImageButton contact,chatroom,sender;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);//去掉标题栏  
 		setContentView(R.layout.activity_com1314080901203);
-	}
+		contact=(ImageButton)findViewById(R.id.id_tab_contact_img);
+		chatroom=(ImageButton)findViewById(R.id.id_tab_chatroom_img);
+		send=(Button)findViewById(R.id.SEND);
+		mViewPager = (ViewPager) findViewById(R.id.id_viewpager);
+        //设置Page间间距
+		mViewPager.setPageMargin(20);
+        //设置缓存的页面数量
+        mViewPager.setOffscreenPageLimit(3);
+        mViewPager.setAdapter(mPagerAdapter = new PagerAdapter()
+        {
+            @Override
+            public Object instantiateItem(ViewGroup container, int position)
+            {
+            	ImageButton view = new ImageButton(Com1314080901203Activity.this);
+                view.setImageResource(imgRes[position]);
+                container.addView(view);
+                return view;
+            }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.com1314080901203, menu);
-		return true;
-	}
+			@Override
+			public int getCount() {
+				// TODO Auto-generated method stub
+				 return imgRes.length;
+			}
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
+			@Override
+			public boolean isViewFromObject(View arg0, Object arg1) {
+				// TODO Auto-generated method stub
+				return arg0==arg1;
+			}
+            
+			 @Override
+	            public void destroyItem(ViewGroup container, int position, Object object)
+	            {
+	                container.removeView((View) object);
+	            }
+
+         });
 	}
+	 @Override
+	    public boolean onOptionsItemSelected(MenuItem item)
+	    {
+		 mViewPager.setAdapter(mPagerAdapter);
+		 return true;
+	    }
+	
 }
