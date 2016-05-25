b7b1fddf2b6399e6b9e226748a3f94d1dc8559f4
LINHZU <771698232@qq.com>
2016-05-25 21:19:49 +0800

    #692  实验4
    图片加代码

---
+++ app/src/main/java/edu/hzuapps/androidworks/homeworks/net1314080903245/Net1314080903145LiveWallpaperSettings.java
@@ -0,0 +1,44 @@
+package edu.hzuapps.androidworks.homeworks.net1314080903145livewallpaper;
+
+
+import android.content.SharedPreferences;
+
+import android.os.Bundle;
+
+import android.preference.PreferenceActivity;
+
+
+public class LiveWallpaperSettings extends PreferenceActivity implements SharedPreferences.
+OnSharedPreferenceChangeListener 
+{
+   
+ @Override
+   
+ protected void onCreate(Bundle savedInstanceState) 
+{
+        super.onCreate(savedInstanceState);
+      
+  getPreferenceManager().setSharedPreferencesName(LiveWallpaper.SHARED_PREFS_NAME);
+     
+   addPreferencesFromResource(R.xml.settings);
+        
+getPreferenceManager().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+  
+  }
+  
+  @Override
+   
+ protected void onDestroy() 
+{
+        
+getPreferenceManager().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+   
+     super.onDestroy();
+     
+ }
+   
+ public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+  
+  }
+
+}
