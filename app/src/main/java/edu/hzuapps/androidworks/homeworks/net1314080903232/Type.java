3c8021f1b563a09e8b200bc24420801534e9a0f7
lxdlxdlxd <1073209655@qq.com>
2016-05-27 19:48:29 +0800

    #90 #94 #95 #7 已完成

---
+++ app/src/main/java/edu/hzuapps/androidworks/homeworks/net1314080903232/Type.java
@@ -0,0 +1,11 @@
+package edu.hzuapps.androidworks.homeworks.net1314080903232;
+
+public class Type {
+	
+	public static final int add=1;
+	public static final int sub=2;
+	public static final int mul=3;
+	public static final int div=4;
+	public static final int num=5;	
+
+}
