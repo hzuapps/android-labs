2dc5b3c587a2c4f6c6d578a0a8021796d4d5a8c7
1040003585 <1040003585@qq.com>
2016-06-06 23:29:54 +0800

    #5 #7 Internet Programming

--- app/src/main/java/edu/hzuapps/androidworks/homeworks/net1314080903133/Net1314080903133MainActivity.java
+++ app/src/main/java/edu/hzuapps/androidworks/homeworks/net1314080903133/Net1314080903133MainActivity.java
@@ -10,18 +10,33 @@ import android.widget.Button;
 public class Net1314080903133MainActivity extends Activity {
 
 	private Button enterEmptyButton;
+	private Button declareButton;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_net1314080903133_main);
 
+		//进入查询空教室按键
 		enterEmptyButton = (Button) findViewById(R.id.button1open);
 		enterEmptyButton.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View view) {
 				Intent intent = new Intent(Net1314080903133MainActivity.this,
 						Net1314080903133EmptyClassroom.class);
+
+				startActivity(intent);
+
+			}
+		});
+		
+		//APP声明按钮
+		declareButton = (Button) findViewById(R.id.button1declare);
+		declareButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				Intent intent = new Intent(Net1314080903133MainActivity.this,
+						Net1314080903133Declare.class);
 
 				startActivity(intent);
 
@@ -37,5 +52,45 @@ public class Net1314080903133MainActivity extends Activity {
 		return true;
 	}
 
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+
+		System.out.println("onResume");
+	}
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+
+		System.out.println("onPause");
+
+	}
+
+	@Override
+	protected void onStop() {
+		// TODO Auto-generated method stub
+		super.onStop();
+
+		System.out.println("onStop");
+	}
+
+	@Override
+	protected void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+
+		System.out.println("onDestroy");
+	}
+
+	@Override
+	protected void onRestart() {
+		// TODO Auto-generated method stub
+		super.onRestart();
+
+		System.out.println("onRestart");
+	}
 
 }
