e35254139e89f80bcc48f4a7a057d08e8c4e0ccd
qingtian1993 <15767979110@163.com>
2016-06-07 21:16:36 +0800

    #93 #43 获取并解析Json数据
    修改上一次Manifest的冲突，并获取并解析Json数据

--- app/src/main/java/edu/hzuapps/androidworks/homeworks/com1314080901225/Com1314080901225GetJsonActivity.java
+++ app/src/main/java/edu/hzuapps/androidworks/homeworks/com1314080901225/Com1314080901225GetJsonActivity.java
@@ -1,7 +1,12 @@
 package com.example.drawingboard;
 
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -11,10 +16,14 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.HttpHeaderParser;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
+
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.View;
@@ -22,63 +31,129 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class Com1314080901225GetJsonActivity extends Activity {
-
-	public RequestQueue requestQueue;
+	
+	private final int SUCCESS = 1;
+	private final int FAILURE = 0;
+	private final int ERRORCODE = 2;
 	private TextView tv;
-
-	@Override
+	
+	private Handler handler=new Handler(){
+		public void handleMessage(Message msg){
+			switch (msg.what) {
+			case SUCCESS:
+				String result=getJson(msg.obj.toString());
+				//主线程更新UI
+				tv.setText(result);
+				Toast.makeText(Com1314080901225GetJsonActivity.this, "获取数据成功", Toast.LENGTH_SHORT)
+				.show();
+				break;
+			case FAILURE:
+				Toast.makeText(Com1314080901225GetJsonActivity.this, "获取数据失败", Toast.LENGTH_SHORT)
+				.show();
+			case ERRORCODE:
+				Toast.makeText(Com1314080901225GetJsonActivity.this, "获取的CODE码不为200！",
+						Toast.LENGTH_SHORT).show();
+			default:
+				break;
+			}
+		}
+	};
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_com1314080901225_get_json);
-		requestQueue = Volley.newRequestQueue(this);
-		tv = (TextView) findViewById(R.id.tv);
+		tv=(TextView) findViewById(R.id.tv);
 	}
-
-	public void click(View v) {
-		String path = "https://github.com/hzuapps/android-labs/blob/master/app/src/main/java/edu/hzuapps/androidworks/homeworks/com1314080901225/city.html";
-		// String path="http://192.168.1.146:8080/city.html"
-
-		JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(path, null,
-				new Response.Listener<JSONObject>() {
-
-					@Override
-					public void onResponse(JSONObject response) {
-						// TODO Auto-generated method stub
-						// System.out.println(response.toString());
-						tv.setText(response.toString());
-					}
-				}, new Response.ErrorListener() {
-
-					@Override
-					public void onErrorResponse(VolleyError error) {
-						// TODO Auto-generated method stub
-						Toast.makeText(getApplicationContext(), "获取数据失败", 0)
-								.show();
-					}
-				})
-		// 返回的json数据是默认使用 ISO-8859-1编码模式，有乱码
-		// 需要重写parseNetworkResponse这个方法解决乱码问题
-		{
-			protected Response<JSONObject> parseNetworkResponse(
-					NetworkResponse response) {
-				JSONObject jsonObject;
+	
+	public String getJson(String string){
+		JSONObject jObject= null;
+		try {
+			jObject =new JSONObject(string);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println("创建JsonObject失败");
+		}
+		//解析JSON数据
+		JSONObject jsonObject = jObject.optJSONObject("weatherinfo");
+		String city=jsonObject.optString("city");
+		String city_en=jsonObject.optString("city_en");
+		String date_y=jsonObject.optString("date_y");
+		String week=jsonObject.optString("week");
+		int cityid=jsonObject.optInt("cityid");
+		String temp1=jsonObject.optString("temp1");
+		System.out.println(temp1+""+date_y+""+city_en+""+cityid+""+week+""+city);
+		String weatherResult="城市："+city+"\n城市英文名："+city_en+"\n日期："+date_y+"\n星期："+week+"\n城市编号："+cityid+"\n温度："+temp1;
+		return weatherResult;
+	}
+	
+	public void click(View v){
+		//开子线程获取网络请求
+		new Thread(){
+			int code=0;
+			public void run() {
+				// TODO Auto-generated method stub
 				try {
-					jsonObject = new JSONObject(new String(response.data,
-							"UTF-8"));
-					return Response.success(jsonObject,
-							HttpHeaderParser.parseCacheHeaders(response));
-				} catch (UnsupportedEncodingException e) {
+					//String path="http://192.168.0.143:8080/city.html";
+					String path="https://raw.githubusercontent.com/hzuapps/android-labs/master/app/src/main/java/edu/hzuapps/androidworks/homeworks/com1314080901225/city.html";
+					URL url=new URL(path);
+					HttpURLConnection conn=(HttpURLConnection) url.openConnection();
+					//设置GET请求
+					conn.setRequestMethod("GET");
+					//设置请求超时时间
+					conn.setConnectTimeout(5000);
+					code=conn.getResponseCode();
+					System.out.println(code);
+					//200表示获取请求成功
+					if(code==200){
+						System.out.println("获取数据成功");
+						InputStream is=conn.getInputStream();
+						String result = readMyInputStream(is);
+						
+						//handle Message
+						Message msg=new Message();
+						msg.obj= result;
+						msg.what=SUCCESS;
+						handler.sendMessage(msg);
+					} else {
+						Message msg = new Message();
+						msg.what = ERRORCODE;
+						handler.sendMessage(msg);
+					}
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					System.out.println("获取数据失败");
 					e.printStackTrace();
-					return Response.error(new ParseError(e));
-				} catch (JSONException e) {
-					e.printStackTrace();
-					return Response.error(new ParseError(e));
+					Message msg = new Message();
+					msg.what = FAILURE;
+					handler.sendMessage(msg);
 				}
+				
+					
+				super.run();
 			}
-
-		};
-
-		requestQueue.add(jsonObjectRequest);
+		}.start();
+	}
+	
+	//以流的形式读取数据
+	public String readMyInputStream(InputStream is){
+		byte[] result;
+		try {
+			ByteArrayOutputStream baos=new ByteArrayOutputStream();
+			byte[] buffer=new byte[1024];
+			int len;
+			while((len = is.read(buffer))!= -1){
+				baos.write(buffer,0,len);
+			}
+			is.close();
+			baos.close();
+			result =baos.toByteArray();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			String errorString="获取数据失败";
+			return errorString;
+		}
+		return new String(result);
 	}
 
 }
