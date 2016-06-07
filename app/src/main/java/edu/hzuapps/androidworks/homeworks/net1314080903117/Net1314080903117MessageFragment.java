7c0af3c414dc3bf240c068723c550f03ba0bfebc
unknown <>
2016-06-07 09:52:57 +0800

    #95 exp9 By #239

---
+++ app/src/main/java/edu/hzuapps/androidworks/homeworks/net1314080903117/Net1314080903117MessageFragment.java
@@ -0,0 +1,21 @@
+package edu.hzuapps.androidworks.homeworks.net1314080903117;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * 消息的主界面
+ * @author Charlie
+ *
+ */
+public class Net1314080903117MessageFragment extends Fragment {
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		return inflater.inflate(R.layout.net1314080903117fragment_message,container, false);
+	}
+
+}
