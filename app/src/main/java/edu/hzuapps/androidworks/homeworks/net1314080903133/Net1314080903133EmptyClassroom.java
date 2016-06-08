6500c906d3fd53089774156916b6ea38433e4db9
1040003585 <1040003585@qq.com>
2016-06-08 12:50:42 +0800

    #9 #5 Android Integrative Experiment

--- app/src/main/java/edu/hzuapps/androidworks/homeworks/net1314080903133/Net1314080903133EmptyClassroom.java
+++ app/src/main/java/edu/hzuapps/androidworks/homeworks/net1314080903133/Net1314080903133EmptyClassroom.java
@@ -1,161 +1,126 @@
 package edu.hzuapps.androidworks.homeworks.net1314080903133;
 
-import java.io.BufferedReader;
+import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLConnection;
-import java.util.Date;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
+
 /**
- * 
- Copyright ? 2016 Authors. All rights reserved.
+ * Copyright ? 2016 Authors. All rights reserved.
  *
- * FileName: Net1314080903133EmptyClassroom.java
+ * FileName: .java
  * @author : Wu_Being <1040003585@qq.com>
- * Date/Time: 2016-6-6/下午7:30:18
- * Description: 
+ * Date/Time: 2016-6-8/下午12:44:51
+ * Description:
  */
 public class Net1314080903133EmptyClassroom extends Activity {
 
-	private static Button findButton = null;
-	private static TextView contentstextView2 = null;
+	private static Button findButton = null; // 查询按键
+	private static TextView contentstextView2 = null; // 空教室信息show
+	private static String allemptyclassinfo = null; // 空教室信息
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-
 		setContentView(R.layout.net1314080903133_empty_classroom);
 
 		findButton = (Button) findViewById(R.id.button1Find);
 		contentstextView2 = (TextView) findViewById(R.id.textView2);
 
 		findButton.setOnClickListener(new View.OnClickListener() {
-
 			@Override
 			public void onClick(View arg0) {
-				/*
-				 * try { // URL hzuPeopleAPPUrl = new
-				 * URL("https://www.baidu.com");// // URL hzuPeopleAPPUrl = new
-				 * URL( //
-				 * "http://1.zooo.vipsinaapp.com/miapp/index.php?aid=ks&uid=olOSijlybNLFRSBN7ub12yQQSkxY&svr=1.wechatlab.vipsinaapp.com"
-				 * ); URL hzuPeopleAPPUrl = new URL(
-				 * "http://202.192.224.137/xsxx.aspx?xh=1314080903134");// ok
-				 * BufferedReader bufferedReader = null; try { // InputStream
-				 * inputStream = // hzuPeopleAPPUrl.openStream(); //
-				 * InputStreamReader inputStreamReader = new //
-				 * InputStreamReader( // inputStream); // bufferedReader = new
-				 * // BufferedReader(inputStreamReader);
-				 * 
-				 * bufferedReader = new BufferedReader( new
-				 * InputStreamReader(hzuPeopleAPPUrl .openStream())); String
-				 * readline = null; while ((readline =
-				 * bufferedReader.readLine()) != null) { /** deal with from this
-				 * function
-				 * 
-				 * // DealWithString(readline); System.out.println(readline);
-				 * contentsEditText.setText(readline); } } catch (IOException e)
-				 * { // TODO Auto-generated catch block
-				 * System.out.println("IOException"); e.printStackTrace(); }
-				 * finally { try { System.out
-				 * .println("finally bufferedReader.close();"); if
-				 * (bufferedReader != null) { bufferedReader.close(); } } catch
-				 * (IOException e) { // TODO Auto-generated catch block
-				 * System.out.println("finally IOException");
-				 * e.printStackTrace(); } }
-				 * 
-				 * } catch (MalformedURLException e) { // TODO Auto-generated
-				 * catch block System.out.println("MalformedURLException");
-				 * e.printStackTrace(); }
-				 */
-				
-				new Thread(new Runnable() {
 
+				// 不能在安卓主线程进程网络流操作，要在创建的新线程中执行
+				new Thread(new Runnable() {
+					
 					@Override
 					public void run() {
-						// TODO Auto-generated method stub
-
-						System.out.println("starting.....");
-						int c;
+						
+						//System.out.println("starting.....");
 						InputStream inputStream = null;
+						DataInputStream dataInputStream = null;
 						try {
-
-							URL url = new URL("http://1.zooo.vipsinaapp.com/miapp/index.php?aid=ks&uid=olOSijlybNLFRSBN7ub12yQQSkxY&svr=1.wechatlab.vipsinaapp.com");
+							// 空教室信息来源URL
+							URL url = new URL(
+									"http://1.zooo.vipsinaapp.com/miapp/index.php?aid=ks&uid=olOSijlybNLFRSBN7ub12yQQSkxY&svr=1.wechatlab.vipsinaapp.com");
 							// URL url = new URL("https://www.baidu.com");
-							System.out.println(url);
-//							HttpURLConnection urlConnection = (HttpURLConnection)url.openConnection();
-//							System.out.println(urlConnection);
-//							urlConnection.connect();
-							//inputStream = urlConnection.getInputStream();
+							// System.out.println(url);
+
 							inputStream = url.openStream();
-							String s="...";
-							while ((c = inputStream.read()) != -1) {
-								System.out.print((char) c);
-								s += (char)c;
+							dataInputStream = new DataInputStream(inputStream);
+
+							String line = ""; // 读取网络流的一行数据
+							String info = ""; // 处理网络后的一行数据，存空教室信息
+							while ((line = dataInputStream.readLine()) != null) {
+								// System.out.println(line);
+								info += DealWithString(line);
 							}
-							final String string = s ;
-							
+
+							// UI线程中用到变量allemptyclassinfo，要设为final
+							allemptyclassinfo = info;
+
+							// 修改UI只能在UI线程中
 							runOnUiThread(new Runnable() {
 								public void run() {
-									//只能在ui线程修改ui
-									contentstextView2.setText(string);
-								
+									contentstextView2
+											.setText(allemptyclassinfo);
 								}
 							});
-							
+
 						} catch (MalformedURLException e) {
-							// TODO Auto-generated catch block
 							e.printStackTrace();
 						} catch (IOException e) {
-							// TODO Auto-generated catch block
 							e.printStackTrace();
+							// 异常处理
+							allemptyclassinfo = "\n\n\n   西湖论坛的新浪服务器异常！！！\n\n   请过几天再试试看。";
+							runOnUiThread(new Runnable() {
+								public void run() {
+									contentstextView2
+											.setText(allemptyclassinfo);
+								}
+							});
 						} finally {
-
 							try {
-								if (inputStream != null) {
-									inputStream.close();
+								if (dataInputStream != null) {
+									dataInputStream.close();
 								}
 							} catch (IOException e) {
-								// TODO Auto-generated catch block
 								e.printStackTrace();
-
 							}
-
 						}
 					}
-				}).start();
+				}).start();// Thread()
 
 			}
 
-		});
+		});// setOnClickListener
 
 	}
 
-	public static void DealWithString(String readline) {
+	/**
+	 * 这个方法待改进！！！！！！！！
+	 * 
+	 * @param readline 待处理的字符串
+	 * @return 空教室信息
+	 */
+	public static String DealWithString(String readline) {
 
-		// TODO Auto-generated method stub
-		// if (readline.length() > 2) {
-		// String subString = readline.substring(0, 2);
-		// // System.out.println(subString);
-		// if ("kb".equals(subString)) {
-		// System.out.println(readline);
-		// }
-		// }
-
+		// html流字符中以"kb"开头的都包含空教室信息
 		if (readline.startsWith("kb")) {
 			System.out.println(readline);
+			return readline;
 		}
+		// 如果html流字符没有以"kb"开头,则返回空字符串拼接
+		return "test!\n";
 
 	}
 }
