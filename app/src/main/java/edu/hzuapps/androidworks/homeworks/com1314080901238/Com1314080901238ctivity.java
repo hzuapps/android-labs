5e545c8889365119c47c2e49c52fe67c12b30584
pilimachao <745728619@qq.com>
2016-06-15 00:54:20 +0800

    #89 #210 引用照片

---
+++ app/src/main/java/edu/hzuapps/androidworks/homeworks/com1314080901238/Com1314080901238ctivity.java
@@ -0,0 +1,16 @@
+package com.example.playinggame;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class Com1314080901238ctivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_com1314080901238ctivity);
+    }
+
+
+}
