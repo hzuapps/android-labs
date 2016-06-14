aad79e4641cf96130ddacd1674f1d2331a1b0bdb
1019940946 <1019940946@qq.com>
2016-06-15 00:16:09 +0800

    Add files via upload

---
+++ app/src/main/java/edu/hzuapps/androidworks/homeworks/net1314080903115/db/Net1314080903115_Account.java
@@ -0,0 +1,15 @@
+package edu.hzuapps.androidworks.homeworks.net1314080903115.db;
+
+/**
+ * Created by lzf on 2016/6/14.
+ */
+public class Net1314080903115_Account {
+
+    public int State=0;//0数据库里没数据，1有数据，还没通过网络验证,2通过网络验证
+    public int ID;
+    public String Phone;
+    public String PassWord;
+    public String UserName;
+    public String HeadPortrait;
+    public String NoteTable;
+}
