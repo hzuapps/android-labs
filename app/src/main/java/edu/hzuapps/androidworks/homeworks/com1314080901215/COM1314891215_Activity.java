3249ff16298985af2e7fee0e58fa7d0d7a9bc185
handsome-handsome-handsome-boy <963705781@qq.com>
2016-06-15 14:57:17 +0800

    实验3

---
+++ app/src/main/java/edu/hzuapps/androidworks/homeworks/com1314080901215/COM1314891215_Activity.java
@@ -0,0 +1,14 @@
+package com.example.administrator.com1314080901215activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        ImageView mimageView=(ImageView)findViewById(R.id.mimage);
+    }
+}
